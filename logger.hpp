fa316589b5b568b9e2e0cb6965fdfa99dfb7906d
ᚱᚢᛈᛚᚢᛗᛖ <ruplume@swgunn.com>
2025-07-17 12:15:08 -0700

    Delete logger.hpp

--- logger.hpp
+++
@@ -1,8 +0,0 @@
-#pragma once
-#include <string>
-
-void log_event(const std::string& tag, const std::string& message);
-std::string get_log_filename();
-std::string get_current_timestamp();
-void ensure_log_dir_exists();
-
